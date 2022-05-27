5c663a831f55bc769d83390504baa15ad5dc3cd4
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-27 17:24:17 +0200

    modifiche

---
+++ script.js
@@ -0,0 +1,9 @@
+//recupero elemento da html con una funzione globale
+const gridContainer = document.querySelector(".grid-container")
+
+//creo una griglia 8*8 con delle celle grandi 50px*50px
+//ciclo for per fare le celle che dura 64 (8*8)
+//creare un div per ogni singola cella
+//appendo la cella al contenitore (grid-container)
+
+//Funzione
