a4fe28da0715c6ff2ee3d550daf2f4e3200ccad1
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-27 18:14:54 +0200

    griglia 8*8

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ const gridContainer = document.querySelector(".grid-container")
 //creare un div per ogni singola cella
 //appendo la cella al contenitore (grid-container)
 
-//Funzione
+//Funzione per fare la griglia
+function generaGriglia(xCells, yCells){
+    const numeroCelle = xCells * yCells;
+
+    console.log(numeroCelle);
+    //aggiungo uno stile in-line per fare un calc per avere una griglia 8*8
+    gridContainer.style.width = `calc(var(--cell-size) * ${xCells})`;
+
+for (let i = 0; i < numeroCelle; i++){
+    //creare div per ogni cella
+    const cell = document.createElement("div");
+    //aggiungo la classe creata in css al div prima di appenderlo per non appensantire il broswer
+    cell.classList.add("cell");
+    
+    //aggiungo la cella al gridContainer 
+    gridContainer.append(cell);
+}
+
+}
+
+generaGriglia(8,8);
